4a84deb41db2b9622b9d4c839250c4da352733ce
[umid shamdinli] <[umidash@code.edu.az]>
2022-05-18 13:10:48 +0400

    Api,Domain,Repository layer's structure is complated on Onion Archetecture

---
+++ LikeTourismApi/DomainLayer/Configurations/FamousCityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Configurations
+{
+    public class FamousCityConfiguration : IEntityTypeConfiguration<FamousCity>
+    {
+        public void Configure(EntityTypeBuilder<FamousCity> builder)
+        {
+            builder.Property(m => m.Name).IsRequired().HasMaxLength(100);
+            builder.Property(m => m.Image).IsRequired();
+            builder.Property(m => m.CreateDate).HasDefaultValue(DateTime.Now);
+            builder.Property(m => m.SoftDelete).HasDefaultValue(false);
+        }
+    }
+}
